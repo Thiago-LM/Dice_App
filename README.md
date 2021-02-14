3065ee66ad8d0b7bc2377730cd0929c466c4f79c
Thiago Leal Menezes <thiago.pixelado@gmail.com>
2021-02-13 23:38:40 -0300

    Finalização do app.

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# dice
+# Dice
 
-A new Flutter project.
+A Flutter project for rolling the dices virtually.
+
+<br/>
+<p align="start">
+  <img width="540" height="1110" src="./images/screenshots/Screenshot_1.png">
+</p>
+<br/>
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
+A few resources to get you started:
 
 - [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- [Section 7: Dicee - Building Apps with State](https://github.com/londonappbrewery/Flutter-Course-Resources#section-7-dicee---building-apps-with-state)
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
         appBar: AppBar(
           title: Text('Dice'),
-          backgroundColor: Colors.red,
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void _randomNumber() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: _randomNumber,
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: _randomNumber,
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
